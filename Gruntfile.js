fe7b31c40073519d758b1a3688486a6c11d4e576
Helior Colorado <me@helior.info>
2014-01-05 23:06:54 -0800

    Added htmlmin to Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,11 +24,28 @@ module.exports = function(grunt) {
           dest: 'www/assets/js'
         }]
       }
+    },
+
+    htmlmin: {
+      prod: {
+        options: {
+          collapseWhitespace: true,
+          removeComments: true
+        },
+        files: [
+          {'www/index.html': 'www/index.html'},
+          {'www/about/index.html': 'www/about/index.html'},
+          {'www/contact/index.html': 'www/contact/index.html'},
+          {'www/events/index.html': 'www/events/index.html'},
+          {'www/venue/index.html': 'www/venue/index.html'}
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  grunt.registerTask('prod', ['compass:prod', 'uglify:prod']);
+  grunt.registerTask('prod', ['compass:prod', 'uglify:prod', 'htmlmin:prod']);
 }
