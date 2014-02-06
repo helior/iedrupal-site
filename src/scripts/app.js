5d0eb15c94cb7e1124a1ae7ac5f01556efe097bb
Helior Colorado <me@helior.info>
2014-02-06 05:29:14 -0800

    Will add scrollTo feature later

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,12 @@
 $(function(){
+	/*
+		Menu toggle button for small-screen devices.
+	 */
 	$('.toggle-topbar').click(function() {
 		$('.top-bar').toggleClass('expanded');
 	});
+
+	/*
+		@todo I need a scrollTo to main content for "learn more" buttons over full-sized images.
+	 */
 });
