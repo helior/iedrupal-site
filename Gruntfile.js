3ae9e6e98fb633b867464f469feb4780db76eedb
Helior Colorado <me@helior.info>
2014-02-12 17:33:09 -0800

    Added basic config for Autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,12 @@ module.exports = function(grunt) {
       }
     },
 
+    autoprefixer: {
+      no_dest: {
+        src: 'www/assets/css/app.css'
+      }
+    },
+
     uglify: {
       prod: {
         options: {
@@ -80,11 +86,12 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
 
   grunt.registerTask('prod', ['compass:prod', 'uglify:prod', 'htmlmin:prod', 'imagemin:prod']);
-  grunt.registerTask('dev', ['compass:dev', 'uglify:dev']);
+  grunt.registerTask('dev', ['compass:dev', 'autoprefixer', 'uglify:dev']);
   grunt.registerTask('default', 'Does nothing.', function() {grunt.log.writeln('Available tasks to run are "prod" and "dev".')});
 }
